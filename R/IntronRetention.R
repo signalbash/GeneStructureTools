2823b3133836c06df212f682eddca208fc3f31ca
betsig <b.signal@garvan.org.au>
2017-07-13 11:26:53 +1000

    Updated functions for finding orfs

--- R/IntronRetention.R
+++ R/IntronRetention.R
@@ -90,8 +90,12 @@ addIntronInTranscript <- function(intronRanges = IR_range, flanking_exons, gtf.e
 
     needs_dup <- which(!(intronRanges$new_transcript_id %in% gtf_transcripts$new_transcript_id))
 
-    while(length(needs_dup) > 0){
+    if(length(needs_dup) > 0){
         gtf_transcripts_add <- gtf_transcripts[gtf_transcripts$transcript_id %in% intronRanges$transcript_id[needs_dup]]
+    }
+
+    while(length(needs_dup) > 0){
+        gtf_transcripts_add <- gtf_transcripts_add[gtf_transcripts_add$transcript_id %in% intronRanges$transcript_id[needs_dup]]
         m <- match(gtf_transcripts_add$transcript_id, intronRanges$transcript_id[needs_dup])
         gtf_transcripts_add$new_transcript_id <- paste0(gtf_transcripts_add$transcript_id,"+INTRON ",intronRanges$exon_id[needs_dup][m])
         gtf_transcripts <- c(gtf_transcripts, gtf_transcripts_add)
@@ -109,6 +113,17 @@ addIntronInTranscript <- function(intronRanges = IR_range, flanking_exons, gtf.e
     #join together exons that are not seperated by an intron
     if(glueExons==TRUE){
 
+        # split pos/neg ordering
+        gtf_transcripts_all.neg <-
+            gtf_transcripts_all[which(as.logical(strand(gtf_transcripts_all) == "-"))]
+        order <- order(gtf_transcripts_all.neg$transcript_id,
+                       plyr::desc(gtf_transcripts_all.neg$exon_number))
+        gtf_transcripts_all.neg <- gtf_transcripts_all.neg[order]
+
+        gtf_transcripts_all.pos <-
+            gtf_transcripts_all[which(as.logical(strand(gtf_transcripts_all) == "+"))]
+        gtf_transcripts_all <- c(gtf_transcripts_all.pos,gtf_transcripts_all.neg)
+
         #extend starts <---<---<---
         w <- which(end(ranges(gtf_transcripts_all))[-length(gtf_transcripts_all)] == start(ranges(gtf_transcripts_all[-1])))
         gtf_transcripts_all <- gtf_transcripts_all
@@ -116,7 +131,7 @@ addIntronInTranscript <- function(intronRanges = IR_range, flanking_exons, gtf.e
             GenomicRanges::start(GenomicRanges::ranges(gtf_transcripts_all))[w]
 
         # remove exons that cover now redundant regions
-        overlaps <- findOverlaps(gtf_transcripts_all.dup, type="within")
+        overlaps <- findOverlaps(gtf_transcripts_all, type="within")
         overlaps <- overlaps[overlaps@from == overlaps@to - 1]
         rm <- unique(overlaps@from)
         if(length(rm) >0){
@@ -139,117 +154,10 @@ addIntronInTranscript <- function(intronRanges = IR_range, flanking_exons, gtf.e
             gtf_transcripts_all <- gtf_transcripts_all[-rm]
         }
 
+        #order <- order(gtf_transcripts_all$transcript_id, gtf_transcripts_all$exon_number)
+        #gtf_transcripts_all <- gtf_transcripts_all[order]
+
     }
     return(gtf_transcripts_all)
 }
 
-
-#' Find the largest distance between two vectors of numbers
-#' Helper function for get_orfs
-#' @param start_site vector of start sites - i.e Met amino acid positions
-#' @param stop_site vector of stop sites - i.e Stop (*) amino acid positions
-#' @param longest 1
-#' @return sequential start site and end site with the greatest difference
-#' @export
-#' @import plyr
-#' @examples
-#' @author Beth Signal
-maxLocation <- function(start_site, stop_site, longest = 1){
-    if(length(start_site) > 0){
-        diffs <- unlist(lapply(start_site, function(x) stop_site[which(stop_site > x)[1]] - x))
-        order <- order(diffs, decreasing=TRUE)
-        max_loc <- order[longest]
-        start <- start_site[max_loc]
-        stop <- stop_site[which(stop_site > start)[1]]
-        return(c(start,stop))
-    }else{
-        return(c(NA,NA))
-    }
-}
-
-#' get distance to second exon-exon junction
-#' @param diffs distances to junctions
-#' @return distance to closest second exon-exon junction
-#' @export
-#' @examples
-#' @author Beth Signal
-distance_to_junction_b <- function(diffs){
-    keep <- which(diffs <= 0)
-    if(length(keep) > 1){
-        return(min(abs(diffs[diffs <= 0])))
-    }else{
-        return(NA)
-    }
-}
-
-#' Get open reading frames for transcripts
-#' @param transcripts GRanges object with ONLY exon annotations (no gene, transcript, CDS etc.) with all transcripts for orf retrevial
-#' @param BSgenome BSgenome object
-#' @param returnLongestOnly TRUE - currently only returns longest ORF
-#' @return data.frame with longest orf details
-#' @export
-#' @import GenomicRanges
-#' @import Biostrings
-#' @examples
-#' @author Beth Signal
-get_orfs <- function(transcripts, BSgenome = g, returnLongestOnly=TRUE){
-
-    transcripts$exon_number <- as.numeric(transcripts$exon_number)
-    order <- order(transcripts$transcript_id, transcripts$exon_number)
-    transcripts <- transcripts[order]
-    transcripts$seq <- as.character(Biostrings::getSeq(g, transcripts))
-
-    seq_cat <- aggregate(seq ~ transcript_id, mcols(transcripts), function(x) (paste(x, collapse="")))
-    ids <- as.character(seq_cat$transcript_id)
-    seq_cat <- seq_cat$seq
-
-    # 3 frames
-    seq_cat <- c(seq_cat, str_sub(seq_cat, 2), str_sub(seq_cat, 3))
-    frames <- rep(c(1,2,3), each = length(ids))
-    ids <- c(ids,ids,ids)
-
-    orf <- suppressWarnings(unlist(lapply(seq_cat, function(x) as.character(Biostrings::translate(Biostrings::DNAString(x))))))
-
-    orf_df <- data.frame(id = ids,
-                         aa_sequence = orf,
-                         frame = frames,
-                         stringsAsFactors = FALSE
-    )
-
-    orf_df$seq_length <- nchar(orf_df$aa_sequence)
-
-    start_sites <- str_locate_all(orf_df$aa_sequence, "M")
-    start_sites <-
-        lapply(start_sites, function(x) as.numeric(x[,2]))
-    stop_sites <- str_locate_all(orf_df$aa_sequence, "[*]")
-    stop_sites <-
-        mapply(function(x,y) c(as.numeric(x[,2]), nchar(y)), stop_sites, orf_df$aa_sequence)
-
-    max_loc <- mapply(function(x,y) maxLocation(x,y), start_sites, stop_sites)
-
-    orf_df$start_site <- max_loc[1,]
-    orf_df$stop_site <- max_loc[2,]
-    orf_df$orf_sequence <- str_sub(orf_df$aa_sequence, orf_df$start_site, orf_df$stop_site - 1)
-    orf_df$orf_length <- nchar(orf_df$orf_sequence)
-
-    widths <- data.frame(w = width(transcripts),
-                         id = transcripts$transcript_id)
-    widths <- aggregate(w ~ id, widths, function(x) cumsum(c(1,x))[-1])
-
-    m <- match(orf_df$id, widths$id)
-    diffs <- mapply(function(x , y) x*3 - y, orf_df$stop_site, widths$w[m])
-
-    orf_df$min_dist_to_junction_a <- suppressWarnings(unlist(lapply(diffs, function(x) min(x[x >= 0]))))
-    orf_df$min_dist_to_junction_a[is.infinite(orf_df$min_dist_to_junction_a)] <- NA
-    orf_df$min_dist_to_junction_b <- unlist(lapply(diffs, distance_to_junction_b)) - orf_df$frame
-    orf_df$aa_sequence <- NULL
-
-    if(returnLongestOnly == TRUE){
-        orf_df <- plyr::arrange(orf_df, plyr::desc(orf_length))
-        orf_df <- orf_df[!duplicated(orf_df$id),]
-    }
-
-    orf_df <- plyr::arrange(orf_df, id)
-    return(orf_df)
-
-}
--- R/longestORF.R
+++ R/longestORF.R
@@ -52,3 +52,179 @@ longestORF <- function(nt_sequence){
     }
     return(longest_all)
 }
+
+
+#' Find the largest distance between two vectors of numbers
+#' Helper function for get_orfs
+#' @param start_site vector of start sites - i.e Met amino acid positions
+#' @param stop_site vector of stop sites - i.e Stop (*) amino acid positions
+#' @param longest how many pairs to return
+#' @return sequential start site and end site with the greatest difference
+#' @export
+#' @import plyr
+#' @examples
+#' @author Beth Signal
+maxLocation <- function(start_site, stop_site, longest = 1){
+    if(length(start_site) > 0){
+        #diffs <- unlist(lapply(start_site, function(x) stop_site[which(stop_site > x)[1]] - x))
+        #order <- order(diffs, decreasing=TRUE)
+        #max_loc <- order[longest]
+        #start <- start_site[max_loc]
+        #stop <- stop_site[which(stop_site > start)[1]]
+        #return(c(start,stop))
+
+        # make start / stop pairs
+        stop_pair_index <- unlist(lapply(start_site, function(x) which.max(1/(stop_site - x))))
+        pairs <- data.frame(start=start_site, stop=stop_site[stop_pair_index])
+        pairs$len <- pairs$stop - pairs$start
+        pairs <- plyr::arrange(pairs, plyr::desc(len))
+        pairs <- pairs[!duplicated(pairs$stop),]
+        return(as.numeric(pairs[longest,1:2]))    }else{
+            return(c(NA,NA))
+        }
+}
+
+#' Get open reading frames for transcripts
+#' @param transcripts GRanges object with ONLY exon annotations (no gene, transcript, CDS etc.) with all transcripts for orf retrevial
+#' @param BSgenome BSgenome object
+#' @param returnLongestOnly only return longest ORF?
+#' @param all_frames return longest ORF for all 3 frames?
+#' @param longest return x longest ORFs (regardless of frames)
+#' @return data.frame with longest orf details
+#' @export
+#' @import GenomicRanges
+#' @import Biostrings
+#' @import stringr
+#' @examples
+#' @author Beth Signal
+get_orfs <- function(transcripts, BSgenome = g, returnLongestOnly=TRUE, all_frames=FALSE, longest=1){
+
+    if(all_frames==TRUE){
+        returnLongestOnly=FALSE
+        longest=1
+    }
+
+
+    # check -ve dist to junction b calls
+    # check exon number ORF start/ends
+
+    transcripts$exon_number <- as.numeric(transcripts$exon_number)
+    order <- order(transcripts$transcript_id, transcripts$exon_number)
+    transcripts <- transcripts[order]
+    transcripts$seq <- as.character(Biostrings::getSeq(g, transcripts))
+
+    seq_cat <- aggregate(seq ~ transcript_id, mcols(transcripts), function(x) (paste(x, collapse="")))
+    ids <- as.character(seq_cat$transcript_id)
+    seq_cat <- seq_cat$seq
+
+    # 3 frames
+    seq_cat <- c(seq_cat, str_sub(seq_cat, 2), str_sub(seq_cat, 3))
+    frames <- rep(c(1,2,3), each = length(ids))
+    ids <- c(ids,ids,ids)
+
+    orf <- suppressWarnings(unlist(lapply(seq_cat, function(x) as.character(Biostrings::translate(Biostrings::DNAString(x))))))
+
+    orf_df <- data.frame(id = ids,
+                         aa_sequence = orf,
+                         frame = frames,
+                         stringsAsFactors = FALSE
+    )
+
+    orf_df$seq_length <- nchar(orf_df$aa_sequence)
+
+    start_sites <- stringr::str_locate_all(orf_df$aa_sequence, "M")
+    start_sites <-
+        lapply(start_sites, function(x) as.numeric(x[,2]))
+    stop_sites <- str_locate_all(orf_df$aa_sequence, "[*]")
+    stop_sites <-
+        mapply(function(x,y) c(as.numeric(x[,2]), nchar(y)), stop_sites, orf_df$aa_sequence)
+
+    max_loc <- mapply(function(x,y) maxLocation(x,y), start_sites, stop_sites)
+
+    if(longest >= 2 & returnLongestOnly == FALSE){
+        orf_df_longest <- orf_df
+
+        for(i in 2:longest){
+            max_loc <- cbind(max_loc,
+                             mapply(function(x,y) maxLocation(x,y, longest = i),
+                                    start_sites, stop_sites))
+            orf_df_longest <- rbind(orf_df_longest, orf_df)
+        }
+
+        o <- order(max_loc[2,] - max_loc[1,],decreasing=TRUE)
+
+        orf_df_longest$start_site <- max_loc[1,]
+        orf_df_longest$stop_site <- max_loc[2,]
+
+        orf_df_longest <- orf_df_longest[o,]
+        keep <- which(!duplicated(orf_df_longest$id))
+        orf_df <- orf_df_longest[keep,]
+        orf_df_longest <- orf_df_longest[-keep,]
+
+        for(i in 2:longest){
+            keep <- which(!duplicated(orf_df_longest$id))
+            orf_df <- rbind(orf_df,orf_df_longest[keep,])
+            orf_df_longest <- orf_df_longest[-keep,]
+        }
+
+    }else{
+        orf_df$start_site <- max_loc[1,]
+        orf_df$stop_site <- max_loc[2,]
+    }
+
+    orf_df$orf_sequence <- stringr::str_sub(orf_df$aa_sequence, orf_df$start_site, orf_df$stop_site - 1)
+    orf_df$orf_length <- nchar(orf_df$orf_sequence)
+
+    orf_df$start_site_nt <- orf_df$start_site*3 - 3 + orf_df$frame
+    orf_df$stop_site_nt <- orf_df$stop_site*3 - 1 + orf_df$frame
+    widths <- data.frame(w = width(transcripts),
+                         id = transcripts$transcript_id)
+    pad <- max(table(widths$id))
+    if(length(unique(transcripts$transcript_id)) == 1){
+        w <- cumsumANDpad(widths$w, pad)
+        diffs <- lapply(orf_df$stop_site_nt, function(x) x - w)
+        diffs <- matrix(unlist(diffs), ncol=length(orf_df$id))
+    }else{
+        #widths_w <- aggregate(w ~ id, widths, function(x) cumsum(c(1,x))[-1])
+        widths_w2 <- aggregate(w ~ id, widths, function(x) cumsumANDpad(x, pad_length = pad))
+        m <- match(orf_df$id, widths_w2$id)
+        widths_w2 <- widths_w2[m,-1]
+        widths_w2  <- split(widths_w2, seq(nrow(widths_w2)))
+        diffs <- mapply(function(x , y) x - y, orf_df$stop_site_nt, widths_w2)
+    }
+
+    orf_df$min_dist_to_junction_a <- suppressWarnings(apply(diffs, 2,function(x) min(x[x > 0 & !is.na(x)])))
+    orf_df$min_dist_to_junction_a[which(is.infinite(orf_df$min_dist_to_junction_a))] <- NA
+    orf_df$exon_a_from_start <- (apply(diffs, 2,function(x) length(x[x > 0 & !is.na(x)]))) - 1
+
+    orf_df$min_dist_to_junction_b <- suppressWarnings((apply(diffs, 2,function(x) max(x[x <= 0 & !is.na(x)])) *-1)+1)
+    orf_df$min_dist_to_junction_b[which(is.infinite(orf_df$min_dist_to_junction_b))] <- NA
+    orf_df$exon_b_from_final <- (apply(diffs, 2,function(x) length(x[x <= 0 & !is.na(x)]))) - 1
+
+    orf_df$aa_sequence <- NULL
+
+    if(returnLongestOnly == TRUE){
+        orf_df <- plyr::arrange(orf_df, plyr::desc(orf_length))
+        orf_df <- orf_df[!duplicated(orf_df$id),]
+    }
+
+    orf_df <- plyr::arrange(orf_df, id)
+    return(orf_df)
+
+}
+
+#' Cumulative sum of a sequence of numbers, padded with NA
+#' @param x input numeric vector
+#' @param pad_length length to pad output to
+#' @return vector with cumulative sum, padded with NA
+#' @export
+#' @examples
+#' @author Beth Signal
+cumsumANDpad <- function(x, pad_length){
+    y <- cumsum(c(1,x))[-1]
+    if(length(y) < pad_length){
+        y <- c(y, rep(NA, pad_length - length(y)))
+    }
+    return(y)
+}
+
