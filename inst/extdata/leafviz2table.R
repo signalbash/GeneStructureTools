906bd19d160ad29804debd1ea755bce3f6a61e4e
betsig <b.signal@garvan.org.au>
2018-04-20 14:53:29 +1000

    Add FDR to intron results in leafviz2table script

--- inst/extdata/leafviz2table.R
+++ inst/extdata/leafviz2table.R
@@ -1,5 +1,7 @@
 #!/usr/bin/env Rscript
 args = commandArgs(trailingOnly=TRUE)
 load(args[1])
+m = match(introns$clusterID, clusters$clusterID)
+introns$FDR = clusters$FDR[m]
 write.table(introns, file=args[2], quote=FALSE, row.names=FALSE, sep="\t")
 
